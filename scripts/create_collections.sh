9dbd94de3218950c6ca9838c78077599accbdeac
Wojciech Ziółek <wojciech.ziolek@cern.ch>
2016-11-25 13:48:02 +0100

    scripts: New collections definitions

--- scripts/create_collections.sh
+++ scripts/create_collections.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 ## User visible collections
 scoap3 collections create scoap3
-scoap3 collections create "Acta Physica Polonica B" -p scoap3 -q 'collections.secondary:Acta'
+scoap3 collections create "Acta Physica Polonica B" -p scoap3 -q 'publication_info.journal_title:"Acta Physica Polonica B"'
 scoap3 collections create "Nuclear Physics B" -p scoap3 -q 'publication_info.journal_title:"Nuclear Physics B"'
 scoap3 collections create "Physics Letters B" -p scoap3 -q 'publication_info.journal_title:"Physics letters B"'
-scoap3 collections create "Advances in High Energy Physics" -p scoap3 -q 'collections.secondary:Hindawi'
+scoap3 collections create "Advances in High Energy Physics" -p scoap3 -q 'publication_info.journal_title:"Advances in High Energy Physics"'
 scoap3 collections create "Chinese Physics C" -p scoap3 -q 'publication_info.journal_title:"Chinese Phys. C"'
-scoap3 collections create "Journal of Cosmology and Astroparticle Physics" -p scoap3 -q 'publication_info.journal_title:JCAP'
+scoap3 collections create "Journal of Cosmology and Astroparticle Physics" -p scoap3 -q 'publication_info.journal_title:"Journal of Cosmology and Astroparticle Physics"'
 scoap3 collections create "New Journal of Physics" -p scoap3 -q 'publication_info.journal_title:"New J. Phys."'
-scoap3 collections create "Progress of Theoretical and Experimental Physics" -p scoap3 -q 'publication_info.journal_title:PTEP'
-scoap3 collections create "Journal of High Energy Physics" -p scoap3 -q 'collections.secondary:SISSA'
-scoap3 collections create "European Physical Journal C" -p scoap3 -q 'collections.secondary:Springer'
+scoap3 collections create "Progress of Theoretical and Experimental Physics" -p scoap3 -q 'publication_info.journal_title:"Progress of Theoretical and Experimental Physics"'
+scoap3 collections create "Journal of High Energy Physics" -p scoap3 -q 'publication_info.journal_title:"Journal of High Energy Physics"'
+scoap3 collections create "European Physical Journal C" -p scoap3 -q 'publication_info.journal_title:"European Physical Journal C"'
 
 ## Hidden collections
 scoap3 collections create scoap3_hidden
