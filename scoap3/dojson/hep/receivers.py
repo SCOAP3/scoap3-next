bc3e86cfc3a862cfbb71e8c154c3f02ec1766d1a
Wojciech Ziółek <wojciech.ziolek@cern.ch>
2018-01-24 16:48:36 +0100

    dosjon: receivers update to support newer version of one of invenio packages

--- scoap3/dojson/hep/receivers.py
+++ scoap3/dojson/hep/receivers.py
@@ -30,48 +30,49 @@ from scoap3.utils.dedupers import dedupe_list_of_dicts
 
 @before_record_insert.connect
 @before_record_update.connect
-def earliest_date(sender, *args, **kwargs):
+def earliest_date(sender, record, *args, **kwargs):
     """Find and assign the earliest date to a HEP paper."""
     dates = []
 
-    if 'preprint_date' in sender:
-        dates.append(sender['preprint_date'])
+    if 'preprint_date' in record:
+        dates.append(record['preprint_date'])
 
-    if 'thesis' in sender:
-        for thesis_key in sender['thesis']:
+    if 'thesis' in record:
+        for thesis_key in record['thesis']:
             if 'date' in thesis_key:
                 dates.append(thesis_key['date'])
             if 'defense_date' in thesis_key:
                 dates.append(thesis_key['defense_date'])
 
-    if 'publication_info' in sender:
-        for publication_info_key in sender['publication_info']:
+    if 'publication_info' in record:
+        for publication_info_key in record['publication_info']:
             if 'year' in publication_info_key:
                 dates.append(publication_info_key['year'])
 
-    if 'creation_modification_date' in sender:
-        for date in sender['creation_modification_date']:
+    if 'creation_modification_date' in record:
+        for date in record['creation_modification_date']:
             if 'creation_date' in date:
                 dates.append(date['creation_date'])
 
-    if 'imprints' in sender:
-        for imprint in sender['imprints']:
+    if 'imprints' in record:
+        for imprint in record['imprints']:
             if 'date' in imprint:
                 dates.append(imprint['date'])
 
     earliest_date = create_earliest_date(dates)
     if earliest_date:
-        sender['earliest_date'] = earliest_date
+        record['earliest_date'] = earliest_date
 
 
 @before_record_insert.connect
 @before_record_update.connect
-def remove_duplicate_authors(sender, *args, **kwargs):
+def remove_duplicate_authors(sender, record, *args, **kwargs):
     """Remove duplicate authors.
 
     Has to be done here rather than when importing the record
     for performance reasons."""
-    try:
-        sender['authors'] = dedupe_list_of_dicts(sender['authors'])
-    except KeyError:
-        pass
+    pass
+    # try:
+    #     record['authors'] = dedupe_list_of_dicts(record['authors'])
+    # except KeyError:
+    #     pass
