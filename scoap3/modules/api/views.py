7c266e76b55ea6f75616f2af13464e3c257f31eb
Wojciech Ziółek <wojciech.ziolek@cern.ch>
2018-07-05 17:25:20 +0200

    api: updates

--- scoap3/modules/api/views.py
+++ scoap3/modules/api/views.py
@@ -1,11 +1,11 @@
 from __future__ import absolute_import, print_function
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 blueprint = Blueprint(
     'scoap3_api',
     __name__,
-    url_prefix='/api',
+    url_prefix='/partners',
     template_folder='templates',
     static_folder='static',
 )
@@ -16,4 +16,12 @@ def index():
     return render_template(
         'scoap3_api/index.html',
         title='SCOAP3 Repository - API'
+    )
+
+
+@blueprint.route('/register', ['GET', 'POST'])
+def register():
+    return render_template(
+        'scoap3_api/register.html',
+        title='SCOAP3 Repository - Tools registration'
     )
