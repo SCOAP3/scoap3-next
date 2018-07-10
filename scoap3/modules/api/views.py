2429203ee2506c09ed01b39f339c8935c599091d
Wojciech Ziółek <wojciech.ziolek@cern.ch>
2018-07-10 14:42:13 +0200

    api: registrtion update (still not working)

--- scoap3/modules/api/views.py
+++ scoap3/modules/api/views.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, print_function
 
 from flask import Blueprint, render_template
+from flask import request
 
 blueprint = Blueprint(
     'scoap3_api',
@@ -11,6 +12,30 @@ blueprint = Blueprint(
 )
 
 
+class EmailRegisterdException(Exception):
+    pass
+
+
+class NameUsedException(Exception):
+    pass
+
+
+def check_registration_parameters(args):
+    pass
+
+
+def save_new_registration():
+    pass
+
+
+def create_new_invenio_user():
+    pass
+
+
+def send_notification_email_to_new_user(uid):
+    pass
+
+
 @blueprint.route('/')
 def index():
     return render_template(
@@ -19,9 +44,23 @@ def index():
     )
 
 
-@blueprint.route('/register', ['GET', 'POST'])
+@blueprint.route('/register', methods=['GET', 'POST'])
 def register():
+    error_message = ''
+    if request.method == 'POST':
+        try:
+            check_registration_parameters(request.args)
+        except EmailRegisterdException():
+            error_message = "This email is already registered."
+        except NameUsedException():
+            error_message = "User with this name: {} already registered".format(request.args.get('inputName'))
+        else:
+            save_new_registration()
+            uid = create_new_invenio_user()
+            send_notification_email_to_new_user(uid)
+
     return render_template(
         'scoap3_api/register.html',
-        title='SCOAP3 Repository - Tools registration'
+        title='SCOAP3 Repository - Tools registration',
+        error_message=error_message
     )
