5178ade0330831dec65fd75fe491922b11513c4a
Wojciech Ziółek <wojciech.ziolek@cern.ch>
2018-08-16 14:09:40 +0200

    api: registration form and admin view

--- scoap3/modules/api/admin.py
+++ scoap3/modules/api/admin.py
@@ -6,62 +6,91 @@
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-"""Admin views for managing access to actions."""
+"""Admin views for managing API registrations."""
 
-from flask import current_app
+from flask import current_app, flash, request
+from flask_admin import expose
+from flask_admin.actions import action
 from flask_admin.contrib.sqla import ModelView
 from werkzeug.local import LocalProxy
 from wtforms import SelectField
 
 from .models import ApiRegistrations
-from .proxies import current_access
 from invenio_db import db
+from invenio_access.proxies import current_access
+from gettext import ngettext
 
 _datastore = LocalProxy(lambda: current_app.extensions['security'].datastore)
 
 
-# def _(x):
-#     Identity.
-#     return x
-
-
 class ApiRegistrationsView(ModelView):
-    """View for managing access to actions by users."""
+    """View for managing access to API registration."""
 
     can_view_details = True
     can_edit = False
-    list_all = ('id', 'creation_date', 'name', 'organization', 'email', 'role', 'country', 'description', 'accepted')
-    column_list = list_all
+
+    #list_all = ('id', 'creation_date', 'name', 'partner', 'organization', 'email', 'role', 'country', 'description', 'accepted',)
+    column_list = ('id', 'creation_date', 'name', 'partner', 'organization', 'email', 'role', 'country', 'description', 'accepted')
 
     column_default_sort = ('id', True)
 
-    # column_labels = {
-    #     'creation_date': _("Registration date"),
-    #     'name': _("Name"),
-    #     'email': _("E-mail"),
-    # }
+    column_labels = {
+         'creation_date': "Registration date",
+         'name': "Name",
+         'email': "E-mail",
+    }
 
-    #column_filters = list_all
+    list_template = 'scoap3_api/custom_list.html'
+    column_filters = column_list
 
-    #form_columns = ('accepted')
+    @action('accept', 'Accept','Are you sure you want to accept selected API registrations?')
+    def action_accept(self, ids):
+        """Accept users."""
+        try:
+            count = 0
+            for api_user_id in ids:
+                if not ApiRegistrations.accept(api_user_id):
+                    raise ValueError("Cannot find API user registration.")
+                else:
+                    count += 1
+            db.session.commit()
+            if count > 0:
+                flash('API user(s) were successfully accepted.', 'success')
+        except Exception as exc:
+            if not self.handle_view_exception(exc):
+                raise
 
-    # form_args = dict(
-    #     action=dict(
-    #         choices=LocalProxy(lambda: [
-    #             (action, action) for action in current_access.actions.keys()
-    #         ])
-    #     )
-    # )
-    # form_overrides = dict(
-    #     action=SelectField,
-    # )
+            current_app.logger.exception(str(exc))  # pragma: no cover
+            flash('Failed to accept API users.','error')  # pragma: no cover
+
+
+    @action('reject', 'Reject','Are you sure you want to reject selected API registrations?')
+    def action_accept(self, ids):
+        """Accept users."""
+        try:
+            count = 0
+            for api_user_id in ids:
+                if not ApiRegistrations.reject(api_user_id):
+                    raise ValueError("Cannot find API user registration.")
+                else:
+                    count += 1
+            db.session.commit()
+            if count > 0:
+                flash('API user(s) were successfully rejected.', 'success')
+        except Exception as exc:
+            if not self.handle_view_exception(exc):
+                raise
+
+            current_app.logger.exception(str(exc))  # pragma: no cover
+            flash('Failed to reject API users.','error')  # pragma: no cover
+
 
 api_registrations_adminview = {
-    'view_class': ApiRegistrations,
-    'args': [ApiRegistrationsView, db.session],
-    'kwargs': {'category': 'Api Registrations'},
+    'model': ApiRegistrations,
+    'modelview':ApiRegistrationsView,
+    'category': 'Api Registrations',
 }
 
 __all__ = (
-    'api_registrations_adminview'
+    'api_registrations_adminview',
 )
--- scoap3/modules/api/views.py
+++ scoap3/modules/api/views.py
@@ -1,7 +1,10 @@
 from __future__ import absolute_import, print_function
 
 from flask import Blueprint, render_template, request
-#from .models import ApiRegistration
+from .models import ApiRegistration
+
+import sys
+from invenio_db import db
 
 
 blueprint = Blueprint(
@@ -50,15 +53,24 @@ def register():
     message = ''
     if request.method == 'POST':
         try:
-            check_registration_parameters(request.args)
-            save_new_registration()
-            uid = create_new_invenio_user()
-            send_notification_email_to_new_user(uid)
+            new_reg = ApiRegistrations(partner=bool(int(request.form['partner'])),
+                                       name=request.form['name'],
+                                       email=request.form['email'],
+                                       organization=request.form['organization'],
+                                       role=request.form['role'],
+                                       country=request.form['country'],
+                                       description=request.form['description']
+                                      )
+            db.session.add(new_reg)
+            db.session.commit()
+
             message = ('success', "Registration succesful. We will confirm your registration shortly.")
         except EmailRegisterdException():
             message = ('error', "User with this <b>email</b>: {} is already registered.".format(request.args.get('inputEmail')))
         except NameUsedException():
             message = ('error', "User with this <b>name</b>: {} is already registered".format(request.args.get('inputName')))
+        except Exception():
+            message = ('error', sys.exc_info()[0])
 
     return render_template(
         'scoap3_api/register.html',
