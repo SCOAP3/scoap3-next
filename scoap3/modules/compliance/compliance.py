9cf914a6b23a73c96be0dad4ddd346ca7e0e3640
Tamas Nyiri <tamas.nyiri@cern.ch>
2018-09-14 17:01:38 +0200

    compliance: add compliance modul

--- scoap3/modules/compliance/compliance.py
+++ scoap3/modules/compliance/compliance.py
@@ -1,3 +1,4 @@
+import itertools
 import re
 from datetime import timedelta
 from dateutil.parser import parse as parse_date
@@ -17,30 +18,63 @@ def __get_first_doi(obj):
 def __extract_text_as_extra_data(obj):
     # fixme extraction shouldn't happen in article_upload?
     # do extraction only if not done earlier
-    if 'extracted_pdf' in obj.extra_data:
+    if 'extracted_data' in obj.extra_data:
         return
 
-    path = None
     for file in obj.extra_data['files']:
-        if file['filetype'] in ('pdf', 'pdf/a'):
+        # TODO add pdfa extraction and checks as well
+        if file['filetype'] in ('pdf', ):
             path = file['url']
-    obj.extra_data['extracted_pdf'] = extract_text_from_pdf(path)
+            obj.extra_data['extracted_data'] = extract_text_from_pdf(path)
 
 
-def __find_regexp_in_pdf(obj, pattern):
+def __find_regexp(data, patterns):
+    """Finds all matches for given patterns.
+    :return list of all matches.
+    """
+
+    matches = []
+
+    for pattern in patterns:
+        # add the surrounding characters too
+        pattern = "(.{0,10}%s.{0,10})" % pattern
+
+        match = re.findall(pattern, data, re.IGNORECASE | re.DOTALL)
+        for m in match:
+            # it can contain a string or tuple of strings if there is multiple match groups
+            if isinstance(m, tuple):
+                matches.append(m[0])
+            else:
+                matches.append(m)
+
+    return matches
+
+
+def __find_regexp_in_pdf(obj, patterns, forbidden_patterns=None):
+    """
+    Finds all matches for given patterns with surrounding characters.
+    Failes only if there are no matches at all.
+    :param patterns: iterable of string patterns
+    """
     __extract_text_as_extra_data(obj)
 
-    match = re.search(pattern, obj.extra_data['extracted_pdf'], re.IGNORECASE)
+    # first check for forbidden patterns
+    if forbidden_patterns:
+        forbidden_matches =__find_regexp(obj.extra_data['extracted_data'], forbidden_patterns)
+        if forbidden_matches:
+            return False, 'Found forbidden match: "%s"' % '", "'.join(set(forbidden_matches)), None
 
-    if not match:
-        return False, None, None
+    matches = __find_regexp(obj.extra_data['extracted_data'], patterns)
 
-    details = 'Found as "%s"' % match.group(0)
+    if not matches:
+        return False, 'Not found.', None
+
+    details = 'Found as: "%s"' % '", "'.join(set(matches))
     return True, details, None
 
 
 def _files(obj):
-    """ check if it has the necessary files: .xml, .pdf, .pdfa """
+    """check if it has the necessary files: .xml, .pdf, .pdfa """
 
     file_types = [file['filetype'] for file in obj.data['_files']]
 
@@ -57,19 +91,16 @@ def _files(obj):
 
     details += 'Available files: %s' % ', '.join(file_types)
 
-    return ok, details, file_types
+    return ok, details, None
 
 
 def _received_in_time(obj):
-    """ check if publication is not older than 24h """
+    """check if publication is not older than 24h """
     api_url = 'https://api.crossref.org/works/%s'
 
     api_message = requests.get(api_url % __get_first_doi(obj)).json()['message']
 
-    # FIXME published-online only contains date of publication. Should we use that?
-    # api_time = api_message.get('published-online', api_message['created'])
-
-    api_time = parse_date(api_message['created']['date-time'], ignoretz=True)
+    api_time = parse_date(api_message['created'])  # todo check published-online for PTEP
     received_time = parse_date(obj.data['acquisition_source']['date'])
     delta = received_time - api_time
 
@@ -83,35 +114,47 @@ def _received_in_time(obj):
 def _founded_by(obj):
     """check if publication has "Founded by SCOAP3" marking *in pdf(a) file* """
 
-    pattern = '.{0,10}scoap(3){0,1}.{0,10}'
-    return __find_regexp_in_pdf(obj, pattern)
+    patterns = ['scoap3?', ]
+    return __find_regexp_in_pdf(obj, patterns)
 
 
 def _author_rights(obj):
-    # todo has author rights: ?
-    return True, None, None
+    COPYRIGHT = u'\N{COPYRIGHT SIGN}'.encode('utf-8')
+
+    start_patterns = (COPYRIGHT, 'copyright', '/(c/)', )
+
+    needed_patterns = [p + '.{15}' for p in start_patterns]
+
+    forbidden_patterns = ['iop', 'institute of physics', 'elsevier', 'hindawi', 'cas', 'chinese academy of science',
+                          'sissa', 'dpg', 'deutsche physikalische gesellschaft', 'uj', 'jagiellonian university', 'oup',
+                          'oxford university press', 'jps', 'physical society of japan', 'springer', 'sif',
+                          'societa italiana di fisica', ]
+
+    forbidden_patterns = ['.{0, 10}'.join(x) for x in itertools.product(start_patterns, forbidden_patterns)]
+
+    return __find_regexp_in_pdf(obj, needed_patterns, forbidden_patterns)
 
 
 def _cc_licence(obj):
-    # has 'cc-by', 'cc by' or 'creative commons attribution'
-    pattern = '(cc-{0,1}by)|(creative commons attribution)'
-    return __find_regexp_in_pdf(obj, pattern)
+    """check fif publication has 'cc by' or 'creative commons attribution' marking *in pdf(a) file* """
+    patterns = ['cc.?by', 'creative.?commons.?attribution', ]
+    return __find_regexp_in_pdf(obj, patterns)
 
 
 COMPLIANCE_TASKS = [
-    ('files', _files),
-    ('in_time', _received_in_time),
-    ('founded_by', _founded_by),
-    ('author_rights', _author_rights),
-    ('cc_licence', _cc_licence),
+    ('Files', _files),
+    ('Received in time', _received_in_time),
+    ('Founded by', _founded_by),
+    ('Author rights', _author_rights),
+    ('Licence', _cc_licence),
 ]
 
 
 def check_compliance(obj, eng):
     checks = {}
     all_ok = True
-    for name, function in COMPLIANCE_TASKS:
-        ok, details, debug = function(obj)
+    for name, func in COMPLIANCE_TASKS:
+        ok, details, debug = func(obj)
         all_ok = all_ok and ok
         checks[name] = {
             'check': ok,
@@ -124,6 +167,7 @@ def check_compliance(obj, eng):
         'checks': checks,
         'accepted': all_ok
     }
+
     c.results = results
     pid = PersistentIdentifier.get('recid', obj.extra_data['recid'])
     c.id_record = pid.object_uuid
