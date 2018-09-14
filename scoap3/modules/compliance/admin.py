0eb3e62df9cfdbc35594ca74e02d71d8a9b2ce06
Tamas Nyiri <tamas.nyiri@cern.ch>
2018-09-14 17:01:38 +0200

    compliance: add compliance modul

--- scoap3/modules/compliance/admin.py
+++ scoap3/modules/compliance/admin.py
@@ -8,16 +8,63 @@
 
 """Admin views for managing API registrations."""
 
+from flask import flash
+from flask_admin.actions import action
 from flask_admin.contrib.sqla import ModelView
+from flask_admin.model.template import macro
+from invenio_db import db
+
 from .models import Compliance
 
 
 class ComplianceView(ModelView):
     """View for managing Compliance results."""
 
-    can_edit = True
+    can_edit = False
     can_delete = False
-    column_default_sort = ('id', True)
+    can_create = False
+    can_view_details = True
+    column_default_sort = ('created', True)
+
+    column_list = ('publisher', 'created', 'updated', 'doi', 'arxiv', 'accepted', 'results')
+    column_formatters = {
+        'results': macro('render_results'),
+        'doi': macro('render_doi'),
+        'arxiv': macro('render_arxiv'),
+    }
+    column_labels = {
+        'results': 'Problems',
+        'arxiv': 'Arxiv number',
+    }
+    column_sortable_list = ()
+
+    list_template = 'scoap3_compliance/admin/list.html'
+    details_template = 'scoap3_compliance/admin/details.html'
+
+    def action_base(self, ids, action):
+        """Calls given function for all id separately.
+        @:return count of successful actions
+        """
+        count = 0
+        for id in ids:
+            id, _ = id.split(',')
+            if action(id):
+                count += 1
+
+        db.session.commit()
+        return count
+
+    @action('accept', 'Accept', 'Are you sure you want to mark all selected record as accepted?')
+    def action_accept(self, ids):
+        count = self.action_base(ids, Compliance.accept)
+        if count > 0:
+            flash('%d compliance record(s) were successfully accepted.' % count, 'success')
+
+    @action('reject', 'Reject', 'Are you sure you want to mark all selected record as rejected?')
+    def action_reject(self, ids):
+        count = self.action_base(ids, Compliance.reject)
+        if count > 0:
+            flash('%d compliance record(s) were successfully rejected.' % count, 'success')
 
 
 compliance_adminview = {
--- scoap3/modules/compliance/models.py
+++ scoap3/modules/compliance/models.py
@@ -11,9 +11,12 @@
 from __future__ import absolute_import, print_function
 
 from datetime import datetime
+
+from flask import flash
 from invenio_db import db
 from invenio_records.models import RecordMetadata
 from sqlalchemy.dialects import postgresql
+from sqlalchemy.orm.attributes import flag_modified
 from sqlalchemy_utils import JSONType, UUIDType
 
 
@@ -21,18 +24,13 @@ class Compliance(db.Model):
     __tablename__ = 'compliance'
 
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    """Primary key. It allows the other fields to be nullable."""
-
     created = db.Column(db.DateTime, default=datetime.utcnow)
     updated = db.Column(db.DateTime, onupdate=datetime.utcnow)
-
     id_record = db.Column(
         UUIDType,
         db.ForeignKey(RecordMetadata.id),
         primary_key=True
     )
-
-    """Id of the community to which the record is applying."""
     record = db.relationship(RecordMetadata, foreign_keys=[id_record])
 
     results = db.Column(db.JSON().with_variant(
@@ -48,3 +46,50 @@ class Compliance(db.Model):
         default=lambda: dict(),
         nullable=False
     )
+
+    @property
+    def accepted(self):
+        return self.results.get('accepted', False)
+
+    @property
+    def doi(self):
+        return self.record.json['dois'][0]['value']
+
+    @property
+    def publisher(self):
+        return self.record.json['imprints'][0]['publisher']
+
+    @property
+    def arxiv(self):
+        # todo
+        return 'todo'
+
+    @classmethod
+    def accept(cls, id):
+        o = cls.query.filter_by(id=id).one()
+        if o.results['accepted']:
+            flash('Compliance check with id "%s" was already accepted.' % o.id, 'warning')
+            return False
+
+        o.results['accepted'] = True
+
+        # https://bugs.launchpad.net/fuel/+bug/1482658
+        # since dict ref. haven't changed need to manually report the change
+        flag_modified(o, 'results')
+
+        return True
+
+    @classmethod
+    def reject(cls, id):
+        o = cls.query.filter_by(id=id).one()
+        if not o.results['accepted']:
+            flash('Compliance check with id "%s" was already rejected.' % o.id, 'warning')
+            return False
+
+        o.results['accepted'] = False
+
+        # https://bugs.launchpad.net/fuel/+bug/1482658
+        # since dict ref. haven't changed need to manually report the change
+        flag_modified(o, 'results')
+
+        return True
