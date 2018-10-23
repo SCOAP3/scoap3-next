9bb6a99597f316cdaca6766608d6c758ffec7bae
Wojciech Ziółek <wojciech.ziolek@cern.ch>
2018-10-23 09:37:51 +0200

    analysis: adds impact calculation

--- scoap3/modules/analysis/models.py
+++ scoap3/modules/analysis/models.py
@@ -13,6 +13,7 @@ from __future__ import absolute_import, print_function
 from datetime import datetime
 from invenio_db import db
 from sqlalchemy import UniqueConstraint
+from sqlalchemy.dialects import postgresql
 
 
 class Gdp(db.Model):
@@ -36,3 +37,28 @@ class Gdp(db.Model):
     value2 = db.Column(db.Integer, default=0, nullable=False, server_default='0')
     value3 = db.Column(db.Integer, default=0, nullable=False, server_default='0')
     value4 = db.Column(db.Integer, default=0, nullable=False, server_default='0')
+
+
+class ArticlesImpact(db.Model):
+
+    __tablename__ = 'analysis_articles_impact'
+
+    __table_args__ = (UniqueConstraint(
+        'name',
+        name='analysis_gdp_unique'),
+    )
+
+    control_number = db.Column(db.Integer, primary_key=True)
+
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+    updated = db.Column(db.DateTime, onupdate=datetime.utcnow)
+
+    details = db.Column(postgresql.JSONB(none_as_null=True),
+                        nullable=False,
+                        default=[]
+    )
+
+    results = db.Column(postgresql.JSONB(none_as_null=True),
+                        nullable=False,
+                        default=[]
+    )
