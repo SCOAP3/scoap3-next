fb47cb0150a834d37679bfd518878dc1a1d144b8
Tamas Nyiri <tamas.nyiri@cern.ch>
2018-11-05 11:45:57 +0100

    accounts: add login_required decorators

--- scoap3/modules/tools/views.py
+++ scoap3/modules/tools/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask_login import login_required
 from flask_menu import register_menu
 
 blueprint = Blueprint(
@@ -11,5 +12,6 @@ blueprint = Blueprint(
 
 @blueprint.route('/test')
 @register_menu(blueprint, 'general.tools', text='<i class="fa fa-cog fa-fw"></i> Tools')
+@login_required
 def test():
     return render_template('scoap3_tools/test.html')
