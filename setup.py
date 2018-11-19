d8875107115304992cd48f47a6cb8b19f7ae9487
Tamas Nyiri <tamas.nyiri@cern.ch>
2018-11-19 14:03:18 +0100

    workflows: add old workflow removal feature

--- setup.py
+++ setup.py
@@ -124,6 +124,7 @@ setup(
         'invenio_celery.tasks': [
             'robotupload = scoap3.modules.robotupload.tasks',
             'analysis = scoap3.modules.analysis.tasks'
+            'workflows = scoap3.modules.workflows.tasks'
         ],
         'invenio_oauth2server.scopes': [
             'harvesting_read = scoap3.modules.oauth2server.scopes:harvesting_read',
