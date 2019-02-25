7316b9fd14a6aa9f7793a1d1be2913f783c00075
Tamas Nyiri <tamas.nyiri@cern.ch>
2019-02-25 17:21:24 +0100

    refactor test_article_upload

--- tests/unit/test_article_upload.py
+++ tests/unit/test_article_upload.py
@@ -8,34 +8,37 @@ from scoap3.modules.records.util import create_from_json
 from tests.responses import get_response_dir
 
 
-def common(input_json_filename):
+def run_workflow(input_json_filename):
     """Use input_json_filename to load hepcrawl response and to run article_upload workflow."""
 
     file_path = path.join(get_response_dir(), 'hepcrawl', input_json_filename)
     with open(file_path, 'rt') as f:
         json_data = json.loads(f.read())
 
-    workflow_ids = create_from_json({'records': [json_data]}, apply_async=False)
-    for workflow_id in workflow_ids:
-        workflow = Workflow.query.get(workflow_id)
-        assert workflow.status == WorkflowStatus.COMPLETED
+    workflow_id = create_from_json({'records': [json_data]}, apply_async=False)[0]
+    return Workflow.query.get(workflow_id)
 
 
 def test_hindawi():
-    common('hindawi.json')
+    workflow = run_workflow('hindawi.json')
+    assert workflow.status == WorkflowStatus.COMPLETED
 
 
 def test_aps():
-    common('aps.json')
+    workflow = run_workflow('aps.json')
+    assert workflow.status == WorkflowStatus.COMPLETED
 
 
 def test_elsevier():
-    common('elsevier/elsevier.json')
+    workflow = run_workflow('elsevier/elsevier.json')
+    assert workflow.status == WorkflowStatus.COMPLETED
 
 
 def test_springer():
-    common('springer/springer.json')
+    workflow = run_workflow('springer/springer.json')
+    assert workflow.status == WorkflowStatus.COMPLETED
 
 
 def test_oup():
-    common('oup/oup.json')
+    workflow = run_workflow('oup/oup.json')
+    assert workflow.status == WorkflowStatus.COMPLETED
