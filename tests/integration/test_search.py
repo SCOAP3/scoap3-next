ba4d9bb45477245c6c4eae0f882b2fbb2a648420
Tamas Nyiri <tamas.nyiri@cern.ch>
2019-07-22 14:50:27 +0200

    records: lowercase normalizer for country and doi

--- tests/integration/test_search.py
+++ tests/integration/test_search.py
@@ -2,6 +2,7 @@ import json
 from urllib import quote
 
 from inspire_utils.record import get_value
+from invenio_search import current_search_client
 
 
 def test_year_field(app_client, test_record):
@@ -19,7 +20,7 @@ def test_year_field(app_client, test_record):
     assert record['year'] == record['imprints'][0]['date'][:4]
 
 
-def test_case_insensitive_doi(app_client, test_record):
+def test_api_case_insensitive_doi(app_client, test_record):
     doi = get_value(test_record, 'dois[0].value')
 
     # has to be non-uppercase doi for the test
@@ -31,4 +32,19 @@ def test_case_insensitive_doi(app_client, test_record):
     data_original = json.loads(response_original.data)
     data_upper = json.loads(response_upper.data)
 
+    assert data_original['hits'] == data_upper['hits']
+
+
+def test_case_insensitive_doi(test_record):
+    index = 'scoap3-records-record'
+    doi = get_value(test_record, 'dois[0].value')
+    doi_upper = doi.upper()
+
+    # has to be non-uppercase doi for the test
+    assert doi != doi_upper
+
+    data_original = current_search_client.search(body={'query': {'term': {'doi': doi}}}, index=index)
+    data_upper = current_search_client.search(body={'query': {'term': {'doi': doi_upper}}}, index=index)
+
+    assert data_original['hits']
     assert data_original['hits'] == data_upper['hits']
