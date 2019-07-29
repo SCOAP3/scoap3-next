9339dd742d0112a8e51d3a5a817bae7f35187066
Tamas Nyiri <tamas.nyiri@cern.ch>
2019-07-29 16:21:06 +0200

    records: add collections count endpoint

--- scoap3/modules/records/views.py
+++ scoap3/modules/records/views.py
@@ -1,7 +1,36 @@
-from flask import Blueprint
+import json
+from datetime import datetime, timedelta
+
+from flask import Blueprint, current_app
+from invenio_search import current_search_client
 
 blueprint = Blueprint(
     'scoap3_records',
     __name__,
     template_folder='templates',
 )
+
+
+@blueprint.route('/collections_count')
+def collections_count():
+    data = {'other': {},
+            'journals': {}}
+
+    # generate journal statistics
+    journals = current_app.config['JOURNAL_ABBREVIATIONS'].keys()
+    for journal in journals:
+        data['journals'][journal] = current_search_client.count(q='journal:"%s"' % journal)['count']
+
+    # generate past x days statistics
+    dates = {
+        'yesterday': (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d'),
+        'last_30_days': (datetime.now() - timedelta(days=30)).strftime('%Y-%m-%d'),
+        'this_year': datetime.now().year,
+    }
+    for key, date in dates.items():
+        data['other'][key] = current_search_client.count(q='record_creation_date:>=%s' % date)['count']
+
+    # all article number
+    data['other']['all'] = sum(count for _, count in data['journals'].items())
+
+    return json.dumps(data)
--- tests/unit/test_records.py
+++ tests/unit/test_records.py
@@ -1,5 +1,11 @@
+import json
+
+from freezegun import freeze_time
+
 from scoap3.modules.records.admin import RecordsDashboard
 from mock import patch
+
+from scoap3.modules.records.views import collections_count
 
 
 def base_admin_article_check(from_date, expected_flash):
@@ -36,3 +42,47 @@ def test_admin_article_check_invalid_date():
 def test_admin_article_check():
     flash = ("Article check started. The result will be sent out in an email.", 'success')
     assert base_admin_article_check('2019-01-01', flash) is True
+
+
+class MockApp():
+    def __init__(self, config):
+        self.config = config
+
+
+class MockES():
+    es_data = {
+        'journal:"Physical Review D"': 4,
+        'journal:"European Physical Journal C"': 5,
+        'journal:"Nuclear Physics B"': 6,
+        'journal:"Physical Review C"': 7,
+
+        "record_creation_date:>=2019-07-28": 3,
+        "record_creation_date:>=2019-06-29": 10,
+        "record_creation_date:>=2019": 20,
+    }
+
+    def count(self, q=None):
+        return {'count': self.es_data.get(q)}
+
+
+def test_collections_count():
+    config = {"JOURNAL_ABBREVIATIONS": {
+        "Physical Review D": "",
+        "European Physical Journal C": "",
+        "Nuclear Physics B": "",
+        "Physical Review C": ""
+    }}
+
+    with patch('scoap3.modules.records.views.current_app', MockApp(config)), \
+            patch('scoap3.modules.records.views.current_search_client', MockES()), \
+            freeze_time('2019-07-29'):
+        data = json.loads(collections_count())
+
+        assert data["other"]["last_30_days"] == 10
+        assert data["other"]["this_year"] == 20
+        assert data["other"]["yesterday"] == 3
+        assert data["other"]["all"] == 22
+        assert data["journals"]["Physical Review D"] == 4
+        assert data["journals"]["European Physical Journal C"] == 5
+        assert data["journals"]["Nuclear Physics B"] == 6
+        assert data["journals"]["Physical Review C"] == 7
