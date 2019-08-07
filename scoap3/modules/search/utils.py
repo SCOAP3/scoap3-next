0400b47eb33f424de8539a411ab5f1416231cfc0
Tamas Nyiri <tamas.nyiri@cern.ch>
2019-08-07 15:18:37 +0200

    search: add export feature

--- scoap3/modules/search/utils.py
+++ scoap3/modules/search/utils.py
@@ -1,7 +1,11 @@
+import csv
+from StringIO import StringIO
+
 from elasticsearch_dsl import Q
 from flask import current_app, request
 from flask_login import current_user
 from inspire_utils.record import get_value
+from invenio_records_rest.query import default_search_factory
 from invenio_search import RecordsSearch
 
 
@@ -54,3 +58,46 @@ def terms_filter_with_must(field):
         terms = [Q('term', **{field: value}) for value in values]
         return Q('bool', must=terms)
     return inner
+
+
+def search_record_from_request():
+    """
+    Returns ElasticSearch Response instance containing search result.
+
+    The used filters are determined by the HTTP parameters.
+    """
+
+    search_index = current_app.config.get('SEARCH_UI_SEARCH_INDEX')
+    search = Scoap3RecordsSearch(index=search_index)
+    search, _ = default_search_factory(None, search)
+    return search, search.execute()
+
+
+def export_search_result(search, search_result):
+    """
+    Exports ElasticSearch search result set as csv.
+
+    The field are defined by SEARCH_EXPORT_FIELDS config variable. It is possible to select only a subset
+    of these fields using the _source parameter of the search.
+    """
+
+    result = StringIO()
+    cw = csv.writer(result, delimiter=";")
+
+    fields = current_app.config.get('SEARCH_EXPORT_FIELDS')
+    if search._source:
+        updated_fields = []
+
+        for name, field, key in fields:
+            if True in [field in source for source in search._source]:
+                updated_fields.append((name, field, key))
+
+        fields = updated_fields
+
+    cw.writerow([name for name, _, _ in fields])
+
+    for hit in search_result.hits:
+        row = [unicode(get_value(hit.to_dict(), key, '')).encode("utf-8") for _, _, key in fields]
+        cw.writerow(row)
+
+    return result.getvalue()
