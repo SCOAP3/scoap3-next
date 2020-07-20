811fe32ed655d3c8c77e6e3cb3709498d49d1770
Tamas Nyiri <info@nyiritamas.hu>
2020-07-20 15:09:30 +0200

    add email options

--- scoap3/config.py
+++ scoap3/config.py
@@ -493,6 +493,11 @@ WORKFLOWS_UI_REST_FACETS = {
 
 WORKFLOWS_STORAGEDIR = '/tmp/workflows-harvesting'
 
+ARTICLE_UPLOAD_SEND_HALTED_EMAILS = True
+"""
+If True an email will be send if an article_upload workflow gets halted.
+"""
+
 CRAWLER_DATA_TYPE = 'harvesting'
 SCRAPY_FEED_URI = '/eos/project/s/scoap3repo/scrapy_feed.json'
 
@@ -650,6 +655,11 @@ COMPLIANCE_JOURNAL_FILES = {
 """
 Required file types for journals. This is validated in the compliance check.
 The articles are required to have *exactly* these files.
+"""
+
+COMPLIANCE_SEND_FAILED_EMAILS = True
+"""
+If true an email will be sent every time a compliance check doesn't pass.
 """
 
 PARTNER_COUNTRIES = ["Australia", "Austria", "Belgium", "Canada", "China", "CERN",
