66ad43e46ff2102c6bc22997a9b95268a6c25a4f
ErnestaP <ernesta.petraityte@yahoo.com>
2022-11-22 17:34:26 +0100

    parsing all dois from xml

--- scripts/update_iop_arxiv.py
+++ scripts/update_iop_arxiv.py
@@ -1,11 +1,4 @@
 from invenio_search import current_search_client as es
-from inspire_utils.record import get_value
-from datetime import datetime
-import dateutil.parser as parser
-from invenio_pidstore.models import PersistentIdentifier
-from invenio_records.api import Record
-from invenio_db import db
-from jsonschema.exceptions import ValidationError, SchemaError
 from invenio_records_files.api import Record
 import os
 from io import StringIO
@@ -31,65 +24,84 @@ def get_all_IOP_articles_dois_without_arxiv():
             },
             "bool": {
                 "must": {
-                    "range": {"imprints.date": {"gte": "2020-01-31", "lte": "2022-12-31"}}
+                    "range": {"imprints.date": {"gte": "2020-01-01", "lte": "2022-12-31"}}
                 }
             }
         }
     }
-    search_results = es.search(index="scoap3-records-record", body=query)
-    for search_result in search_results["hits"]["hits"]:
-        try:
-            doi = search_result["_source"]["dois"][0]["value"]
-            dois.append(doi)
-        except:
-            pass
+    search_results = es.search(index="scoap3-records-record", scroll='1m', body=query)
+    sid = search_results['_scroll_id']
+    scroll_size = len(search_results['hits']['hits'])
+    while (scroll_size > 0):
+        for record_index in range(scroll_size):
+            try:
+                doi = search_results["hits"]["hits"][record_index]["_source"]["dois"]
+                dois = dois + [doi_['value'] for doi_ in doi]
+            except:
+                pass
+            search_results = es.scroll(scroll_id=sid, scroll='2m')
+            sid = search_results['_scroll_id']
+            scroll_size = len(search_results['hits']['hits'])
     return dois
 
 
-def get_files_paths(dois, pattern_doi):
+def get_arxivs_and_dois(dois):
+    import sys
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+
     path = "/data/harvesting/IOP/download/"
-    xml_file_paths = []
     arxivs = {}
     zips = [os.path.join(path, zip_file) for zip_file in os.listdir(path) if '.zip' in zip_file]
     for zip_file in zips:
         with ZipFile(zip_file, 'r') as zip:
-            # try:
-                name = os.path.basename(zip_file).split('.')[0]
-                all_files_names_in_zip = zip.namelist()
-                for file_name in all_files_names_in_zip:
-                    if 'xml' in file_name:
-                        # data = zip.read(file_name)
-                        # doi_in_data = pattern_doi.search(data).group(1)
-                        # if doi_in_data in dois:
-                        xml = zip.read(file_name)
-                        arxivs[get_arxiv(xml).keys()[0]] = get_arxiv(xml)[get_arxiv(xml).keys()[0]]
-                        xml_file_paths.append(file_name)
-
-            # except:
-            #     pass
+            name = os.path.basename(zip_file).split('.')[0]
+            all_files_names_in_zip = zip.namelist()
+            for file_name in all_files_names_in_zip:
+                if 'xml' in file_name:
+                    data = zip.read(file_name)
+                    element = parse_without_names_spaces(data)
+                    doi = get_doi(element)
+                    if doi in dois:
+                        arxivs[doi] = get_arxiv(element)
+                    else:
+                        query = {
+                            "query": {
+                                "bool": {
+                                    "must": [{"match": {"dois.value": doi}}],
+                                }
+                            }
+                        }
+                        search_result = es.search(index="scoap3-records-record", body=query)
+                        try:
+                            recid = search_result["hits"]["hits"][0]["_source"]["control_number"]
+                        except:
+                            print("No record with doi ", doi )
     return arxivs
 
 
-def parse_without_names_spaces(xml):
-    it = ET.iterparse(StringIO((unicode(xml.encode('utf-8'), 'utf-8'))))
+def parse_without_names_spaces(data):
+    xml = StringIO(unicode(data, "utf-8"))
+    it = ET.iterparse(xml)
     for _, el in it:
         _, _, el.tag = el.tag.rpartition("}")
     root = it.root
     return root
 
 
-def get_arxiv(xml):
-    element = parse_without_names_spaces(xml)
+def get_doi(element):
+    doi = element.find("front/article-meta/article-id/[@pub-id-type='doi']")
+    return doi.text
+
+
+def get_arxiv(element):
     arxiv = element.find(
         "front/article-meta/custom-meta-group/custom-meta/meta-value"
     )
-    doi = element.find("front/article-meta/article-id/[@pub-id-type='doi']")
     if arxiv is not None:
-        return {doi.text: arxiv.text}
-    return {doi.text: None}
+        return arxiv.text
 
 
-pattern_doi = re.compile(r'.*<article-id pub-id-type="doi">(.*?)<\/article-id>.*')
 dois = get_all_IOP_articles_dois_without_arxiv()
-paths = get_files_paths(dois, pattern_doi)
+paths = get_arxivs_and_dois(dois)
 print(paths)
