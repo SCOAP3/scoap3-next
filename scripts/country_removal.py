3d6a2668be9a284c43ea3e0cba89b4d0f673332a
ErnestaP <ernesta.petraityte@yahoo.com>
2023-07-04 10:55:30 +0200

    scoap3-next: country removal ref: https://github.com/cern-sis/issues-scoap3/issues/173

---
+++ scripts/country_removal.py
@@ -0,0 +1,56 @@
+from invenio_search import current_search_client as es
+from invenio_pidstore.models import PersistentIdentifier
+from invenio_records_files.api import Record
+from invenio_db import db
+import re
+
+
+def get_cern_cooperation_agreement():
+    recids = []
+    query = {
+        "_source": ["_files", "control_number"],
+        "query": {
+            "regexp": {
+                "authors.affiliations.value": {
+                    "value": ".*cooperation.*",
+                    "flags": "ALL",
+                    "case_insensitive": "true",
+                }
+                }
+            }
+        }
+
+    search_result = es.search(index="scoap3-records-record", scroll='1m', body=query)
+    sid = search_result['_scroll_id']
+    scroll_size = len(search_result['hits']['hits'])
+    while (scroll_size > 0):
+        for record_index in range(scroll_size):
+            recid = search_result["hits"]["hits"][record_index]["_source"]["control_number"]
+            recids.append(recid)
+        search_result = es.scroll(scroll_id=sid, scroll='2m')
+        sid = search_result['_scroll_id']
+        scroll_size = len(search_result['hits']['hits'])
+    return recids
+
+def remove_country(record):
+    for author in record["authors"]:
+        for affiliation in author["affiliations"]:
+            pattern_for_cern_cooperation_agreement = re.compile(r'cooperation agreement with cern', re.IGNORECASE)
+            match_pattern = pattern_for_cern_cooperation_agreement.search(affiliation["value"])
+            if match_pattern is not None:
+                affiliation.pop('country', None)
+    return record
+
+def update_records(recids):
+    for recid in recids:
+        pid = PersistentIdentifier.get("recid", recid)
+        existing_record = Record.get_record(pid.object_uuid)
+        existing_record = remove_country(existing_record)
+        existing_record.update(dict(existing_record))
+        print('Updating record...', recid)
+        existing_record.commit()
+        db.session.commit()
+
+
+records_with_wrong_aff = get_cern_cooperation_agreement()
+update_records(records_with_wrong_aff)
