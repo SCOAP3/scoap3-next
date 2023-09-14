ddac051caecd32255ddb0b9beff9682a830c451d
ErnestaP <ernesta.petraityte@yahoo.com>
2023-09-14 14:45:53 +0200

    small fix; specified articles which are in prod and qa

--- scripts/reparse_article_authors.py
+++ scripts/reparse_article_authors.py
@@ -6,10 +6,147 @@ from invenio_db import db
 import os
 import re
 
+common = [u'10.1088/1674-1137/ac2a25',
+ u'10.1088/1674-1137/ac2b12',
+ u'10.1088/1674-1137/ac6cd3',
+ u'10.1088/1674-1137/ac6cd5',
+ u'10.1088/1674-1137/ac66cc',
+ u'10.1088/1674-1137/ac67d0',
+ u'10.1088/1674-1137/ac6d4e',
+ u'10.1088/1674-1137/ac68da',
+ u'10.1088/1674-1137/ac6665',
+ u'10.1088/1674-1137/abe19b',
+ u'10.1088/1674-1137/abde2e',
+ u'10.1088/1674-1137/abe195',
+ u'10.1088/1674-1137/abdf43',
+ u'10.1088/1674-1137/abe199',
+ u'10.1088/1674-1137/ac5b0e',
+ u'10.1088/1674-1137/abc244',
+ u'10.1088/1674-1137/abc538',
+ u'10.1088/1674-1137/abc539',
+ u'10.1088/1674-1137/abc682',
+ u'10.1088/1674-1137/abc683',
+ u'10.1088/1674-1137/abc0cc',
+ u'10.1088/1674-1137/abc169',
+ u'10.1088/1674-1137/abc245',
+ u'10.1088/1674-1137/abf13a',
+ u'10.1088/1674-1137/ac0c6f',
+ u'10.1088/1674-1137/ac0ee2',
+ u'10.1088/1674-1137/acaf26',
+ u'10.1088/1674-1137/acc1cf',
+ u'10.1088/1674-1137/ac9896',
+ u'10.1088/1674-1137/abcd90',
+ u'10.1088/1674-1137/ac0ee4',
+ u'10.1088/1674-1137/ac0e88',
+ u'10.1088/1674-1137/ac0e8a',
+ u'10.1088/1674-1137/ac1575',
+ u'10.1088/1674-1137/abccac',
+ u'10.1088/1674-1137/abd16d',
+ u'10.1088/1674-1137/ac68d7',
+ u'10.1088/1674-1137/ac4df1',
+ u'10.1088/1674-1137/ac6490',
+ u'10.1088/1674-1137/ac5fa2',
+ u'10.1088/1674-1137/abdfbe',
+ u'10.1088/1674-1137/abe110',
+ u'10.1088/1674-1137/abe197',
+ u'10.1088/1674-1137/abe19a',
+ u'10.1088/1674-1137/acac6d',
+ u'10.1088/1674-1137/acac69',
+ u'10.1088/1674-1137/acbbc0',
+ u'10.1088/1674-1137/abcc5b',
+ u'10.1088/1674-1137/acc641',
+ u'10.1088/1674-1137/ac1668',
+ u'10.1088/1674-1137/abfe51',
+ u'10.1088/1674-1137/abfc38',
+ u'10.1088/1674-1137/abf828',
+ u'10.1088/1674-1137/abfb50',
+ u'10.1088/1674-1137/abfb5f',
+ u'10.1088/1674-1137/ac9de9',
+ u'10.1088/1674-1137/aca38d',
+ u'10.1088/1674-1137/ac9deb',
+ u'10.1088/1674-1137/ac957b',
+ u'10.1088/1674-1137/ac9895',
+ u'10.1088/1674-1137/aca00d',
+ u'10.1088/1674-1137/ac9897',
+ u'10.1088/1674-1137/aca585',
+ u'10.1088/1674-1137/aca8f6',
+ u'10.1088/1674-1137/ac92d8',
+ u'10.1088/1674-1137/abd084',
+ u'10.1088/1674-1137/abce50',
+ u'10.1088/1674-1137/abd01a',
+ u'10.1088/1674-1137/abcfac',
+ u'10.1088/1674-1137/abe03c',
+ u'10.1088/1674-1137/aca1aa',
+ u'10.1088/1674-1137/ac3fab',
+ u'10.1088/1674-1137/ac21b8',
+ u'10.1088/1674-1137/ac1d9c',
+ u'10.1088/1674-1137/ac224b',
+ u'10.1088/1674-1137/ac1e09',
+ u'10.1088/1674-1137/ac0e8b',
+ u'10.1088/1674-1137/ac1bfd',
+ u'10.1088/1674-1137/ac1b9a',
+ u'10.1088/1674-1137/ac1ef9',
+ u'10.1088/1674-1137/ac4704',
+ u'10.1088/1674-1137/ac4ee8',
+ u'10.1088/1674-1137/ac1c66',
+ u'10.1088/1674-1137/ac4f4c',
+ u'10.1088/1674-1137/ac87f1',
+ u'10.1088/1674-1137/abcfaa',
+ u'10.1088/1674-1137/ac878c',
+ u'10.1088/1674-1137/ac8bc9',
+ u'10.1088/1674-1137/acc3f4',
+ u'10.1088/1674-1137/ac90af',
+ u'10.1088/1674-1137/acc4ab',
+ u'10.1088/1674-1137/abcfab',
+ u'10.1088/1674-1137/abe9a2',
+ u'10.1088/1674-1137/abf9ff',
+ u'10.1088/1674-1137/abe8cf',
+ u'10.1088/1674-1137/abfd28',
+ u'10.1088/1674-1137/ac2f95',
+ u'10.1088/1674-1137/ac2ed1',
+ u'10.1088/1674-1137/ac6573',
+ u'10.1088/1674-1137/ac6cd8',
+ u'10.1088/1674-1137/ac6cd6',
+ u'10.1088/1674-1137/ac6d51',
+ u'10.1088/1674-1137/ac5c2e',
+ u'10.1088/1674-1137/ac6b92',
+ u'10.1088/1674-1137/aca959',
+ u'10.1088/1674-1137/ac9d28',
+ u'10.1088/1674-1137/abce4f',
+ u'10.1088/1674-1137/abd088',
+ u'10.1088/1674-1137/abce10',
+ u'10.1088/1674-1137/abcf22',
+ u'10.1088/1674-1137/abd92a',
+ u'10.1088/1674-1137/ac8651',
+ u'10.1088/1674-1137/abc1d5',
+ u'10.1088/1674-1137/abc242',
+ u'10.1088/1674-1137/abc241',
+ u'10.1088/1674-1137/acc92d',
+ u'10.1088/1674-1137/abeb07',
+ u'10.1088/1674-1137/abf4f6',
+ u'10.1088/1674-1137/abe36d',
+ u'10.1088/1674-1137/abefca',
+ u'10.1088/1674-1137/ac3fac',
+ u'10.1088/1674-1137/ac3fae',
+ u'10.1088/1674-1137/ac3d2b',
+ u'10.1088/1674-1137/ac3faa',
+ u'10.1088/1674-1137/ac061c',
+ u'10.1088/1674-1137/ac88bb',
+ u'10.1088/1674-1137/acaa22',
+ u'10.1088/1674-1137/ac930b',
+ u'10.1088/1674-1137/ac92da',
+ u'10.1088/1674-1137/ac89d1',
+ u'10.1088/1674-1137/ac8cd5',
+ u'10.1088/1674-1137/ac8789',
+ u'10.1088/1674-1137/ac89d0',
+ u'10.1088/1674-1137/aca888',
+ u'10.1088/1674-1137/ac80b4',
+ u'10.1088/1674-1137/ac957c']
+
 def gel_records_from_2020_artids():
     records_ = {}
     query = {
-        "_source": ["publication_info", "control_number"],
+        "_source": ["publication_info", "control_number", "dois"],
         "query": {
             "bool": {
                 "must": [
@@ -37,7 +174,9 @@ def gel_records_from_2020_artids():
         for record_index in range(scroll_size):
             recid = search_result["hits"]["hits"][record_index]["_source"]["control_number"]
             artid = search_result["hits"]["hits"][record_index]["_source"]["publication_info"][0]["artid"]
-            records_[artid] = recid
+            doi = search_result["hits"]["hits"][record_index]["_source"]["dois"][0]["value"]
+            if doi in common:
+                records_[artid] = recid
         search_result = es.scroll(scroll_id=sid, scroll='2m')
         sid = search_result['_scroll_id']
         scroll_size = len(search_result['hits']['hits'])
@@ -148,11 +287,11 @@ class IOPParser():
         rid = reffered_id.get("rid")
         institution = self._get_institution(article, rid)
         country = self._get_country(article, rid)
-        if country:
-            institution_and_country["country"] = country.text
-        if institution and country:
+        if country is not None:
+            institution_and_country["country"] = country.text.decode("ascii")
+        if institution is not None and country is not None:
             institution_and_country["institution"] = ", ".join(
-                [institution.text, country.text]
+                [institution.text.decode("ascii"), country.text.decode("ascii")]
             )
         else:
             return self._get_affiliation(article, rid)
@@ -165,11 +304,14 @@ class IOPParser():
     def _get_country(self, article, id):
         return article.find("./front/article-meta/contrib-group/aff[@id='" + id +"']/country")
 
+    def _get_orcid(self, article, id):
+        return article.find("./front/article-meta/contrib-group/aff[@id='" + id +"']/country")
+
     def _get_affiliation(self, article, id):
         value = ET.tostring(article.find("./front/article-meta/contrib-group/aff[@id='" + id +"']"))
         without_spaces = ' '.join(value.split())
         value =  re.search('</label>(.*)</aff>', without_spaces).group(1).strip()
-        return {'country': value.split(',')[-1], 'value': value}
+        return {'country': value.split(',')[-1].strip(), 'value': value}
 
 
 def parse_authors(recids_and_paths):
@@ -183,7 +325,7 @@ def parse_authors(recids_and_paths):
             parsed_authors_list[recid] = result
     return parsed_authors_list
 
-
+dois = []
 def update_records(data):
     recids = data.keys()
     for recid in recids:
@@ -192,9 +334,10 @@ def update_records(data):
         existing_record['authors'] = data[recid]
         existing_record.commit()
         db.session.commit()
-    print('Updated:', recid)
+        dois.append(existing_record['dois'][0]['value'])
+        print('Updated:', recid, " DOI:", existing_record['dois'][0]['value'])
 
 records = gel_records_from_2020_artids()
 results = get_all_iop_xml_files(records)
-data = parse_authors({"61453":results['61453']})
+data = parse_authors({"61271": results["61271"]})
 update_records(data)
